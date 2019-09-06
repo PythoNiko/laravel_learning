9be5ef4eb6c758abde6259888c3a7f039a50bddb
pythoniko <NGC91@hotmail.co.uk>
2019-09-06 12:55:37 +0100

    Basic routing tutorial

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About</title>
+    <body>
+        <h1>About</h1>
+
+        <p>
+            <a href="/">Home</a>.
+        </p>
+
+    </body>
+
+
+</html>
