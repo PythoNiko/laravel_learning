3886014154f1da56c5e5b4189af9dd35f66a9731
pythoniko <NGC91@hotmail.co.uk>
2019-11-11 13:50:47 +0000

    Playing about with some basic principles

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class PagesController extends Controller{
     public function home(){
@@ -32,5 +33,26 @@ class PagesController extends Controller{
 
     public static function multiplier($a, $b){
         return $c = $a * $b;
+    }
+
+    public static function name_printer($name){
+        return "My name is: " . $name;
+    }
+
+    public static function data_type_printer(){
+        $myString = "This is a string";
+        $myInt = 123;
+        $myFloat = 1.23;
+        $myArray = array( 1, 2.3, "Test", array(1, 2, 3), True);
+        $myBool = True;
+        $myNull = NULL;
+
+        echo "<br>";
+        echo "String: " . $myString . "<br>";
+        echo "Integer: " . $myInt . "<br>";
+        echo "Float: " . $myFloat . "<br>";
+        echo "Array: " . var_dump($myArray) . "<br>";
+        echo "Boolean: " . $myBool . "<br>";
+        echo "Null: " . $myNull . "<br>";
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -13,8 +13,15 @@
 
     <p>
         <?php
-        use App\Http\Controllers\PagesController;
-        echo PagesController::multiplier(2, 3);
+            use App\Http\Controllers\PagesController;
+
+            echo PagesController::multiplier(2, 3);
+            echo "<br>";
+            echo PagesController::multiplier(10, 8);
+            echo "<br>";
+            echo PagesController::name_printer("Niko");
+            echo "<br>";
+            echo PagesController::data_type_printer();
         ?>
     </p>
 @endsection
