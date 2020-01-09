6b317aace22eb49ddfb43fdc56def09a81ad3f58
pythoniko <NGC91@hotmail.co.uk>
2020-01-09 15:31:46 +0000

    Added page for learning PHP

--- resources/views/php.blade.php
+++ resources/views/php.blade.php
@@ -6,6 +6,34 @@
 
 @section('content')
 
-Hello!
+    <?php
+        class Task{
+
+            protected $description;
+            protected $completed = false;
+
+            // constructor
+            public function __construct($description){
+                $this->description = $description;
+            }
+
+            public function isComplete(){
+                return $this->completed;
+            }
+
+            public function complete(){
+                $this->completed = true;
+            }
+        }
+
+        // new task object
+        $task = new Task('Go to the store');
+
+        // complete the task
+        $task->complete();
+
+        // should return true(1)
+        echo $task->isComplete();
+    ?>
 
 @endsection
