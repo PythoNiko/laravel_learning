d6faf6b8977c8035d7d4fd33dfd2e9ee1a05ddc9
Nick.Connell <NGC91@hotmail.co.uk>
2020-08-16 10:41:40 +0100

    Initial Property table added

--- resources/views/techtest/index.blade.php
+++ resources/views/techtest/index.blade.php
@@ -9,6 +9,68 @@
         <h1>Tech Test</h1>
     </div>
 
+    <div class="card mb-40">
+        <div class="cardBody">
+            <h3>Proeprties</h3>
+            <table v-pre class="tableLayoutAuto">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Unit</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th>Start Date</th>
+                    <th>Expiry Date</th>
+                    <th></th>
+                </tr>
+                </thead>
+                @if ($response)
+                    @foreach ($response as $property)
+                        <tr>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                Test1
+                            </td>
+                            <td>
+                                <a href="#">
+                                    <i class="fal fa-edit"></i>
+                                    Edit
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td class="centeredTitleText" colspan="9">
+                            No Properties Found
+                        </td>
+                    </tr>
+                @endif
+            </table>
+            <a href="#" class="button noMarginBottom">
+                Add New Property
+            </a>
+        </div>
+    </div>
+
 {{--    <?php--}}
 {{--        // dd($response);--}}
 {{--        $i = 0;--}}
