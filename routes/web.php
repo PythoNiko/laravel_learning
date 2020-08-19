1b7949d6970382867d707e80c8c78c50161ddb05
Nick.Connell <NGC91@hotmail.co.uk>
2020-08-19 13:36:04 +0100

    Code Improvements

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
- Route::get('/', function() {
+ Route::get('/', function () {
      return '';
  })->name('index');
 
-Route::get('/', 'HomeController@home');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/vue', 'VueController@index');
-Route::get('/php', 'PhpController@index');
+ Route::get('/', 'HomeController@home');
+ Route::get('/home', 'HomeController@index')->name('home');
+ Route::get('/vue', 'VueController@index');
+ Route::get('/php', 'PhpController@index');
 
-Route::resource('tasks', 'TasksController');
-Route::resource('sessions','SessionsController');
-Route::resource('property','PropertyController');
-Route::get('/property-delete', 'PropertyController@removeProperty')->name('property.removeProperty');
+ Route::resource('tasks', 'TasksController');
+ Route::resource('sessions', 'SessionsController');
+ Route::resource('property', 'PropertyController');
+ Route::get('/property-delete', 'PropertyController@removeProperty')->name('property.removeProperty');
